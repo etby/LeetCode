c7a9ac49f860ec8e73367a0b61ff9a522b5b51cc
Etby <etby.org@gmail.com>
2018-04-24 20:17:28 +0800

    ac 101

---
+++ kotlin/src/org/etby/leetcode/easy/_101/Solution.kt
@@ -0,0 +1,42 @@
+package org.etby.leetcode.easy._101
+
+/**
+ * Definition for a binary tree node.
+ * class TreeNode(var `val`: Int = 0) {
+ *     var left: TreeNode? = null
+ *     var right: TreeNode? = null
+ * }
+ */
+class TreeNode(var `val`: Int = 0) {
+    var left: TreeNode? = null
+    var right: TreeNode? = null
+}
+
+class Solution {
+    fun isSymmetric(root: TreeNode?): Boolean {
+        if (root == null) {
+            return true
+        }
+
+        return dfs(root.left, root.right)
+    }
+
+    fun dfs(left: TreeNode?, right: TreeNode?): Boolean {
+        if (left == null && right == null) {
+            return true
+        }
+        if (left == null || right == null) {
+            return false
+        }
+        if (left.`val` != right.`val`) {
+            return false
+        }
+        if (!dfs(left.left, right.right)) {
+            return false
+        }
+        if (!dfs(left.right, right.left)) {
+            return false
+        }
+        return true
+    }
+}
