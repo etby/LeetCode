0d3f855137f2d10e753e1fdfb68a657a419b82e8
Etby <etby.org@gmail.com>
2018-04-25 23:40:22 +0800

    ac 111

---
+++ kotlin/src/org/etby/leetcode/easy/_111/Solution.kt
@@ -0,0 +1,33 @@
+package org.etby.leetcode.easy._111
+
+import org.etby.leetcode.easy.tree.binary.TreeNode
+
+/**
+ * Definition for a binary tree node.
+ * class TreeNode(var `val`: Int = 0) {
+ *     var left: TreeNode? = null
+ *     var right: TreeNode? = null
+ * }
+ */
+class Solution {
+    fun minDepth(root: TreeNode?): Int {
+        if (root == null) {
+            return 0
+        }
+
+        var min = 0
+        var nodes = mutableListOf<TreeNode>()
+        root.left?.let { nodes.add(it) }
+        root.right?.let { nodes.add(it) }
+        nodes.forEach {
+            val depth = minDepth(it)
+            if (depth != 0) {
+                if (min == 0 || depth < min) {
+                    min = depth
+                }
+            }
+        }
+
+        return 1 + min
+    }
+}
