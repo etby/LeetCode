962655b8c369064b69ded60e0c6a7be913a5cf79
Etby <etby.org@gmail.com>
2018-05-04 18:07:45 +0800

    c 141

---
+++ c/easy/_141.c
@@ -0,0 +1,25 @@
+#include <stdbool.h>
+
+#include "singly-linked.h
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+bool hasCycle(struct ListNode *head) {
+    if (!head || !head->next)
+        return false;
+
+    struct ListNode *fast = head;
+    struct ListNode *slow = head;
+
+    do {
+        fast = fast->next->next;
+        slow = slow->next;
+    } while (fast != NULL && fast->next != NULL && fast != slow);
+
+    return fast == slow;
+}
